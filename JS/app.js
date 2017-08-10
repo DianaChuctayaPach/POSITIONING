681f64123d1cbb838fd7cc0eace0606542e6c4f1
diana chuctaya pacheco <leydiana.ch2015@gmail.com>
2017-08-10 16:13:48 -0500

    desafio completado git add .git add .

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+var contenedor = document.getElementsByClassName("contenedor6")[0];
+var rosa = document.getElementById("x6");
+var azul = document.getElementById("y6");
+var verde = document.getElementById("z6");
+var amarillo = document.getElementById("w6");
+// var caja = document.getElementById("caja");
+
+ contenedor.style.position="relative";
+ contenedor.style.background ="Darkgray";
+ contenedor.style.width="600px";
+ contenedor.style.padding = "50px";
+
+ azul.style.position ="absolute";
+ azul.style.background ="blue"
+//  azul.style.width="200px";
+//  azul.style.hedht="200px";
+ azul.style.position = "250px";
+ azul.style.right = "0";
+verde.style.background="green";
